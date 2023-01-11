e12ea489b1be5493e7cb57d94fd819cc4f890966
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-01-11 21:34:00 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <User name="Pedro" age={21} email="pedro@pedro.com" />
+      <User name="Natalie" age={29} email="nat@nat.com" />
+      <User name="Ajay" age={19} email="ajay@ajay.com" />
+    </div>
+  );
 }
+
+const User = ({ name, age, email }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h1>{age}</h1>
+      <h1>{email}</h1>
+    </div>
+  );
+};
 
 export default App;
