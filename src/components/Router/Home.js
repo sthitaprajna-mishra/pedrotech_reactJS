86f486d4039fd442c15512f8f2e736ae15ddf2e2
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-01-15 18:51:48 +0530

    episode 9

--- src/components/Router/Home.js
+++ src/components/Router/Home.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
 
 const Home = () => {
   let navigate = useNavigate();
+
+  const { data, isLoading, isError, refetch } = useQuery(["cat"], () => {
+    return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+  });
+
+  if (isError) return <h2>Error</h2>;
+  if (isLoading) return <h2>Loading...</h2>;
+
   return (
     <div>
       <h2>Home</h2>
       <button onClick={() => navigate("/contact")}>
         Change to contact page
       </button>
+      <h1>Random Cat Fact #n</h1>
+      <h2>{data?.fact}</h2>
+
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 };
--- src/components/Router/MainRouter.js
+++ src/components/Router/MainRouter.js
@@ -3,20 +3,31 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Contact from "./Contact";
 import Profile from "./Profile";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const MainRouter = () => {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
     <div>
-      <Router>
-        <Link to="/"> Home </Link>
-        <Link to="/contact"> Contact </Link>
-        <Link to="/profile"> Profile </Link>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/profile/:username" element={<Profile />} />
-        </Routes>
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Link to="/"> Home </Link>
+          <Link to="/contact"> Contact </Link>
+          <Link to="/profile"> Profile </Link>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/profile/:username" element={<Profile />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 };
